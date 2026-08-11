a784d0205ad682ad68a935e9ce5be9354fd48d60
shalva02 <44312272+shalva02@users.noreply.github.com>
2026-08-12 01:48:50 +0400

    finish etch-a-sketch project

--- javascript.js
+++ javascript.js
@@ -1,8 +1,21 @@
-let gridPrompted = 56;
-let grid = gridPrompted * gridPrompted;
 const container = document.getElementById("container");
-function createGrid() {
-  container.style.setProperty("--columns", gridPrompted);
+const button = document.querySelector("button");
+container.addEventListener("mouseover", (event) => {
+  let target = event.target;
+  target.style.background = "red";
+});
+button.addEventListener("click", (event) => {
+  let userInput = prompt("enter grid size. max: 100");
+  if (userInput > 1 && userInput <= 100) {
+    createGrid(userInput);
+  } else {
+    alert("please enter numbers from 1 to 100");
+  }
+});
+function createGrid(gridNum = 16) {
+  container.style.setProperty("--columns", gridNum);
+  container.textContent = "";
+  let grid = gridNum * gridNum;
   for (i = 0; i < grid; i++) {
     let div = document.createElement("div");
     div.classList.add("grid-items");
